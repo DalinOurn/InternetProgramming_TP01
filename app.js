6af8b792187ca0391725b7f85c5f6e77c26ad771
Dalin Ourn <ourndalin111@gmail.com>
2022-06-22 10:45:22 +0700

    Uppercase PORT

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = require ("express")();
+const port = process.env.PORT || 3000;
+
+app.get("",(req, res)=> {
+    res.send("Hello world, Heroku");
+})
+
+app.get("/api/user", (req, res) => {
+    ""
+})
+
+app.listen(port,() => {
+    console.log('App is running!');
+})
